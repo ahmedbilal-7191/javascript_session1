bf880c7c84b3bc502b4e6016b2c890bdecc5c47c
ahmedbilal-7191 <75791273+ahmedbilal-7191@users.noreply.github.com>
2023-05-15 20:12:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1. Create a variable for each data type (String, Number, Bloat, Boolean).
+var student_name="bilal ahmed"
+var age=19
+var marks=99.9
+var male=true
+// 2) Use all the above declared variable in a console.log and get the output
+console.log("My name is "+student_name + " and i am "+age+" year old "+"having percentage of "+marks+" in B.E ECE")
+console.log(male)
+// 3) Using VAR, LET and CONST, find the solution for the following values. Alse declare the
+// required variables and formular.
+// # Area of a circle
+// # Circumference of a circle
+// # Radius of the circle.
+
+// area of circle 
+let area_of_circle
+const PI=3.14
+var radius_of_circle =4
+area_of_circle=PI*radius_of_circle**2
+console.log("area of circle of radius 4 is " + area_of_circle)
+
+// circumference of circle 
+let circumference_of_circle
+circumference_of_circle=2*PI*radius_of_circle
+console.log("circumference of circle of radius 4 is " + circumference_of_circle)
+
+// radius of circle 
+let radius = (circumference_of_circle)/(2*PI)
+console.log( " Radius of circle is " +radius)
+
